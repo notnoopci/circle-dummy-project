535a659fc372da2e6a1ee3cc17eddc95e1f319d6
Mahmood Ali <mahmood@circleci.com>
2017-07-29 19:10:19 +0800

    fixes

--- sysbench-mysql.sh
+++ sysbench-mysql.sh
@@ -1,5 +1,6 @@
 export MYSQL_USER=${MYSQL_USER:-$(whoami)}
-time sysbench --test=oltp --mysql-table-engine=innodb --oltp-table-size=1000000 --mysql-user=${MYSQL_USER} prepare
-time sysbench --num-threads=4 --max-time=900 --max-requests=500000 --test=oltp --oltp-table-size=80000000 --mysql-user=${MYSQL_USER} --mysql-table-engine=innodb --oltp-test-mode=complex run
-time sysbench --num-threads=4 --max-time=900 --max-requests=500000 --test=oltp --oltp-table-size=80000000 --mysql-user=${MYSQL_USER} --mysql-table-engine=innodb --oltp-test-mode=complex cleanup
+export ADDITIONAL_ARGS=${ADDITIONAL_ARGS:-}
 
+time sysbench --test=oltp --mysql-table-engine=innodb --oltp-table-size=1000000 --mysql-user=${MYSQL_USER} $ADDITIONAL_ARGS prepare
+time sysbench --num-threads=4 --max-time=900 --max-requests=500000 --test=oltp --oltp-table-size=80000000 --mysql-user=${MYSQL_USER} --mysql-table-engine=innodb --oltp-test-mode=complex $ADDITIONAL_ARGS run
+time sysbench --num-threads=4 --max-time=900 --max-requests=500000 --test=oltp --oltp-table-size=80000000 --mysql-user=${MYSQL_USER} --mysql-table-engine=innodb --oltp-test-mode=complex $ADDITIONAL_ARGS cleanup
